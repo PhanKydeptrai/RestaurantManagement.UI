eaeee82e655750fe5fcbe5bacbaa8e352c8a1408
TuanWorkSpace <tuanworkspace@gmail.com>
2024-12-15 01:38:44 +0700

    Add account verification and result pages to transaction flow

--- RestaurantManagement.AdminUI/src/Controller.tsx
+++ RestaurantManagement.AdminUI/src/Controller.tsx
@@ -50,6 +50,10 @@ import TransactionPage from "./pages/transacsion-page/transacsion";
 import TransactionErrorPage from "./pages/transacsion-page/transactionerror";
 import TransactionCompletePage from "./pages/transacsion-page/transactioncomplete";
 import ChangeTablePage from "./pages/order-page/changetable";
+import VerifyAccountPage from "./pages/transacsion-page/verifyaccount";
+import VerifyAccountCompletePage from "./pages/transacsion-page/verifyaccountcomplete";
+import ChangePasswordResultPage from "./pages/transacsion-page/changepasswordresult";
+import TransactionResultEmailPage from "./pages/transacsion-page/transactionresultemail";
 
 function Controller() {
     return (
@@ -58,6 +62,11 @@ function Controller() {
                 <Route path="/" element={<Login />} />
                 <Route path="/donetransaction" element={<TransactionCompletePage />} />
                 <Route path="/errortransaction" element={<TransactionErrorPage />} />
+
+                <Route path="/verifyaccount" element={<VerifyAccountPage />} />
+                <Route path="/verifyaccountcomplete" element={<VerifyAccountCompletePage />} />
+                <Route path="/changepasswordresult" element={<ChangePasswordResultPage />} />
+                <Route path="/transactionresult" element={<TransactionResultEmailPage />} />
                 <Route path="/dashboard" element={
                     <ProtectedRoute requiredRoles={['Boss', 'Manager']}>
                         <Home />
---
+++ RestaurantManagement.AdminUI/src/pages/transacsion-page/transactionresultemail.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Button, Result } from 'antd';
+
+const TransactionResultEmailPage = () => (
+    <Result
+        status="success"
+        title="Yêu cầu của bạn đã được gửi thành công"
+        subTitle="Vui lòng kiểm tra email để xem kết quả"
+
+    />
+);
+
+export default TransactionResultEmailPage;
